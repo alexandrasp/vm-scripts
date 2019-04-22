085893538913e9e2a5756dd5eab1c97139774cc5
André Almeida <andrealmeid@riseup.net>
2019-04-22 15:09:29 -0300

    Add strace and gdb to packs
    Signed-off-by: André Almeida <andrealmeid@riseup.net>

--- vm/deploy_vm.sh
+++ vm/deploy_vm.sh
@@ -2,7 +2,7 @@
 disk_space='5G'
 disk_name='arch_disk.raw'
 
-extra_packs='vim fish git'
+extra_packs='vim fish git strace gdb'
 mug_packs='python2 graphviz evince'
 v4l2_packs='qt5-base glu gstreamer gst-plugins-base gst-plugins-good'
 xforwarding_packs='xorg-xauth xorg-xclock openssh xorg-fonts-type1 haveged'
@@ -10,7 +10,7 @@ xforwarding_packs='xorg-xauth xorg-xclock openssh xorg-fonts-type1 haveged'
 all_packs="$extra_packs $mug_packs $v4l2_packs $xforwarding_packs"
 
 # allocate and format disk file
-# dd if=/dev/zero of=$disk_name bs=512MB count=8
+# dd if=/dev/zero of=$disk_name bs=512MB count=10
 # truncate -s $disk_space $disk_name
 mkfs.ext4 $disk_name
 
