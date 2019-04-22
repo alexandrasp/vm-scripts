9bbeeb78ecf72f5c2505f720b63e6d7a7d13246c
André Almeida <andrealmeid@riseup.net>
2019-04-22 15:10:05 -0300

    Rename vars
    Signed-off-by: André Almeida <andrealmeid@riseup.net>

--- test_vimc/test_mplanar.sh
+++ test_vimc/test_mplanar.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-FRAME_DIR=/root/host/scripts/test_vimc/frames
+frame_dir=/root/host/scripts/test_vimc/frames
+config_top="python2 mug-config_topology.py"
 
 echo "Multiplanar format render tests"
 
+$config_top total_top
 cd /root/host/v4l-grab
 rm frame.*
 ./grab
 
 echo "Testing YVU420M"
-diff -q frame.yv12 $FRAME_DIR/frame.yv12
+diff -q frame.yv12 $frame_dir/frame.yv12
 
 echo "Testing NV12M"
-diff -q frame.nv12 $FRAME_DIR/frame.nv12
+diff -q frame.nv12 $frame_dir/frame.nv12
