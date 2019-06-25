8b38cb8b2f26715575f9887798cacc4a90a0ee4d
André Almeida <andrealmeid@riseup.net>
2019-06-25 11:22:05 -0300

    Update tests

---
+++ test_vimc/config_vimc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+media-ctl -d platform:vimc -V '"Sensor A":0[fmt:SBGGR8_1X8/640x480]'
+media-ctl -d platform:vimc -V '"Debayer A":0[fmt:SBGGR8_1X8/640x480]'
+media-ctl -d platform:vimc -V '"Sensor B":0[fmt:SBGGR8_1X8/640x480]'
+media-ctl -d platform:vimc -V '"Debayer B":0[fmt:SBGGR8_1X8/640x480]'
+v4l2-ctl -z platform:vimc -d "RGB/YUV Capture" -v width=1920,height=1440
+v4l2-ctl -z platform:vimc -d "Raw Capture 0" -v pixelformat=BA81
+v4l2-ctl -z platform:vimc -d "Raw Capture 1" -v pixelformat=BA81
+v4l2-ctl -z platform:vimc -e "RGB/YUV Input" -v width=640,height=480
