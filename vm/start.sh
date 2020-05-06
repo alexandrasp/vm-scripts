329d1d1b8b71b429ffa27dd9cd78615a97275dbe
André Almeida <andrealmeid@riseup.net>
2020-05-06 17:33:40 +0000

    start: add create user

--- vm/start.sh
+++ vm/start.sh
@@ -9,6 +9,13 @@ systemctl start $services
 mkdir .ssh
 cat id_rsa.pub > .ssh/authorized_keys
 
+# create a user
+useradd -m user -G wheel
+mkdir /home/user/.ssh
+cat id_rsa.pub > /home/user/.ssh/authorized_keys
+echo "fish" >> /home/user/.bashrc
+echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
 # fill the fstab
 echo "shared_folder /root/host 9p trans=virtio 0 0" >> /etc/fstab
 
