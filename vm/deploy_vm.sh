fadb196c004380399f23e9393fbed16a3eb16ee7
Alexandra Pereira <alexandra.pereira@collabora.com>
2020-08-14 22:55:12 -0300

    load right disk on debian

--- vm/deploy_vm.sh
+++ vm/deploy_vm.sh
@@ -17,8 +17,12 @@ sudo mount $disk_name mnt
 sleep 3
 
 # install basic system
-sudo debootstrap stable mnt http://deb.debian.org/debian/
-sudo chroot mnt apt-get -y install $all_packs 
+sudo debootstrap stable mnt http://ftp.br.debian.org/debian/
+sudo chroot mnt apt-get -y install $all_packs
+sudo add-apt-repository deb "http://ftp.de.debian.org/debian buster main non-free"
+sudo apt-get update
+sudo apt-get install firmware-linux
+sudo update-initramfs -c -k all 
 
 # configure ssh
 sudo cp ~/.ssh/id_rsa.pub mnt/root/
--- vm/run_vm.sh
+++ vm/run_vm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 qemu-system-x86_64 \
-	-boot order=a -drive file=debian.vm,format=raw,if=virtio \
+	-boot order=a -hda debian.vm \
 	-nographic -kernel \
  	/home/alexandra/dev/collabora/linux/arch/x86/boot/bzImage \
-	-append "root=/dev/vda rw console=ttyS0 loglevel=7 raid=noautodetect" \
+	-append "root=/dev/sda rw console=ttyS0 loglevel=7 raid=noautodetect" \
 	-fsdev local,id=fs1,path=/home/alexandra/dev/collabora,security_model=none \
 	-device virtio-9p-pci,fsdev=fs1,mount_tag=shared_folder \
 	--enable-kvm -m 2G -smp 2 -cpu host \
